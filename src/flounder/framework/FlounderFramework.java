0b7d4895d4a81b3a32639689f6be6eac52de0ab6
Matthew Albrecht <mattparks5855@gmail.com>
2017-02-04 16:08:41 -0600

    Biomes

--- src/flounder/framework/FlounderFramework.java
+++ src/flounder/framework/FlounderFramework.java
@@ -52,7 +52,7 @@ public class FlounderFramework extends Thread {
 		loadFlounderStatics(unlocalizedName);
 
 		// Increment revision every fix for the minor version release. Minor version represents the build month. Major incremented every two years OR after major core framework rewrites.
-		this.version = new Version("21.01.10");
+		this.version = new Version("04.02.10");
 
 		// Sets basic framework info.
 		this.closedRequested = false;
