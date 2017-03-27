e3dc339792e376d05ba38abd89ddc816031bb392
Matthew Albrecht <mattparks5855@gmail.com>
2017-03-27 12:25:41 -0500

    Gui and water tweaks and listeners.

---
+++ src/flounder/events/EventChange.java
@@ -0,0 +1,43 @@
+package flounder.events;
+
+/**
+ * A class that acts as a basic change listener for a value.
+ *
+ * @param <T> The type of value to find change with.
+ */
+public abstract class EventChange<T> implements IEvent {
+	private ValueReference<T> reference;
+	private T current;
+
+	/**
+	 * Creates a new change event.
+	 *
+	 * @param reference The reference to listen to.
+	 */
+	public EventChange(ValueReference<T> reference) {
+		this.reference = reference;
+	}
+
+	@Override
+	public boolean eventTriggered() {
+		T newValue = reference.get();
+		boolean triggered = !newValue.equals(current);
+		current = newValue;
+		return triggered;
+	}
+
+	@Override
+	public abstract void onEvent();
+
+	/**
+	 * A reference to a value.
+	 */
+	public interface ValueReference<T> {
+		/**
+		 * Gets the value.
+		 *
+		 * @return The value.
+		 */
+		T get();
+	}
+}
