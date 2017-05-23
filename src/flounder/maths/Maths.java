d6ecf9fca8f46a2f3ba3853dc7de4a6f94ced548
Matthew Albrecht <mattparks5855@gmail.com>
2017-05-23 11:57:14 +0000

    Maths that makes no sense.

--- src/flounder/maths/Maths.java
+++ src/flounder/maths/Maths.java
@@ -637,4 +637,10 @@ public class Maths {
 
 		return result;
 	}
+
+
+	public static float smoothlyStep(float edge0, float edge1, float x) {
+		float t = clamp((x - edge0) / (edge1 - edge0), 0.0f, 1.0f);
+		return t * t * (3.0f - 2.0f * t);
+	}
 }
